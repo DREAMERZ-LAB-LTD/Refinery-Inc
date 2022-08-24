a75c386d7f6c3ac3182e87dd130800836abe849c
olyislam <32776200+olyislam@users.noreply.github.com>
2022-08-24 17:29:57 +0600

    Fixed upgradeable status refresh issue

--- Assets/Idle Arcade Core/Scripts/Core/ContainerStatusUI.cs
+++ Assets/Idle Arcade Core/Scripts/Core/ContainerStatusUI.cs
@@ -4,22 +4,43 @@ namespace IdleArcade.Core
 {
     public class ContainerStatusUI : MonoBehaviour
     {
+        [SerializeField] private string upgradebaleID;
         [SerializeField] protected TransactionContainer container;
         [SerializeField] protected TextMeshProUGUI statusText;
         [Header("Text Message")]
         [SerializeField] private string startMessage;
         [SerializeField] private string centertMessage = " / ";
         [SerializeField] private string endMessage;
-        protected virtual void Awake()
+
+        protected virtual void Start()
         {
             container.OnChangedValue += OnContainerUpdate;
+            if (upgradebaleID != string.Empty)
+            {
+                var upgradeableData = UpgradeSystem.instance.GetDataField(upgradebaleID);
+                if (upgradeableData != null)
+                    upgradeableData.OnChanged += OnUpgrade;
+            }
         }
 
         protected virtual void OnDestroy()
         { 
             container.OnChangedValue -= OnContainerUpdate;
+            if (upgradebaleID != string.Empty)
+            {
+                if (UpgradeSystem.instance)
+                { 
+                    var upgradeableData = UpgradeSystem.instance.GetDataField(upgradebaleID);
+                    if (upgradeableData != null)
+                        upgradeableData.OnChanged -= OnUpgrade;
+                }
+            }
         }
 
+        private void OnUpgrade(float t)
+        { 
+            container.Add(0);//just refresh all of the listner to upto date the updated status
+        }
 
         protected virtual void OnContainerUpdate(int delta, int currnet, int max, string containerID, TransactionContainer A, TransactionContainer B)
         {
