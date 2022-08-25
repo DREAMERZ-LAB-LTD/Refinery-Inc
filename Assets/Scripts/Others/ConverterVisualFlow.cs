9efb27cf5290396734a3a33e39c687b9625990e1
olyislam <32776200+olyislam@users.noreply.github.com>
2022-08-25 19:09:07 +0600

    Improved Machine visual flow

--- Assets/Scripts/Others/ConverterVisualFlow.cs
+++ Assets/Scripts/Others/ConverterVisualFlow.cs
@@ -5,37 +5,65 @@ using IdleArcade.Core;
 public class ConverterVisualFlow : MonoBehaviour, TransactionConverter.IConverter
 {
     [SerializeField, Range(0f, 1f)] private float processingDelay = 0.5f;
+    [SerializeField] private Entity fromPrefab;
     [SerializeField] private Entity convertedPrefab;
     [SerializeField] private Transform A;
     [SerializeField] private Transform B;
-    private GameObject visualFlow;
+    [SerializeField] private Transform C;
+    private GameObject fromVisualElement;
+    private GameObject toVisualElement;
 
     private void Awake()
     {
-        visualFlow = Instantiate(convertedPrefab.gameObject, transform.position, transform.rotation, transform);
-        visualFlow.SetActive(false);
+        fromVisualElement = Instantiate(fromPrefab.gameObject, A.position, A.rotation, transform);
+        fromVisualElement.SetActive(false);
+        toVisualElement = Instantiate(convertedPrefab.gameObject, B.position, B.rotation, transform);
+        toVisualElement.SetActive(false);
     }
     public void OnConvertBegin(float delay)
     {
-        StartCoroutine(MoveTo(A.position, B.position));
+        StartCoroutine(MoveTo(A.position, B.position, C.position));
 
-        IEnumerator MoveTo(Vector3 from, Vector3 to)
+        IEnumerator MoveTo(Vector3 A,Vector3 B, Vector3 C)
         {
             var processingTime = delay * processingDelay;
-            if (processingTime > 0)
-                yield return new WaitForSeconds(processingTime);
 
+            var swipeDuration = delay - processingTime;
+            var totalPathLength = (C - A).magnitude;
+
+            var pathLength = (A - B).magnitude;
+            var dt = Mathf.InverseLerp(0, totalPathLength, pathLength);
             var startTime = Time.time;
-            var endTime = startTime + (delay - processingTime);
-            float t = 0;
-            visualFlow.SetActive(true);
+            var endTime = startTime + dt * swipeDuration;
+
+            float t;
+            fromVisualElement.SetActive(true);
             while (Time.time < endTime)
             {
                 t = Mathf.InverseLerp(startTime, endTime, Time.time);
-                visualFlow.transform.position = Vector3.Lerp(from, to, t);
+                fromVisualElement.transform.position = Vector3.Lerp(A, B, t);
                 yield return null;
             }
-            visualFlow.SetActive(false);
+            fromVisualElement.SetActive(false);
+
+            if (processingTime > 0)
+                yield return new WaitForSeconds(processingTime);
+            else
+                yield return null;
+
+            pathLength = (B - C).magnitude;
+            dt = Mathf.InverseLerp(0, totalPathLength, pathLength);
+            startTime = Time.time;
+            endTime = startTime + dt * swipeDuration;
+
+            toVisualElement.SetActive(true);
+            while (Time.time < endTime)
+            {
+                t = Mathf.InverseLerp(startTime, endTime, Time.time);
+                toVisualElement.transform.position = Vector3.Lerp(B, C, t);
+                yield return null;
+            }
+            toVisualElement.SetActive(false);
         }
     }
 
--- Assets/Scripts/Others/PlayerCarController.cs
+++ Assets/Scripts/Others/PlayerCarController.cs
@@ -6,7 +6,6 @@ public class PlayerCarController : MonoBehaviour
     [SerializeField] private float moveSpeed = 10;
     [SerializeField] private float maxVelocity = 3;
     [SerializeField] private float turnSpeed = 1;
-    [SerializeField] private float maxTrackdOffset = 3;
 
     private Rigidbody rb;
     private Joystick joystick;
@@ -27,45 +26,29 @@ public class PlayerCarController : MonoBehaviour
     {
         if (joystick == null || rb == null)
             return;
-
-        velocity = rb.velocity;
         if (joystick.Horizontal == 0 && joystick.Vertical == 0)
         {
-            moveDir = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
-            velocity.y = 0;
-            if (Vector3.Dot(velocity.normalized, moveDir) > 0.01f)
-            {
-                velocity = moveDir * velocity.magnitude;
-                velocity.y = rb.velocity.y;
-                rb.velocity = velocity;
-            }
-            var friction = 1 - Time.fixedDeltaTime * 5;
-            rb.velocity = new Vector3(rb.velocity.x * friction, rb.velocity.y, rb.velocity.z * friction);
-            rb.angularVelocity *= friction;
+            rb.velocity *= 0.8f;
+            return;
         }
-        else
-        { 
-            inputDir = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
-            rb.rotation = Quaternion.Lerp(rb.rotation, Quaternion.LookRotation(inputDir), turnSpeed * Time.fixedDeltaTime);
 
-            moveDir = new Vector3(transform.forward.x, 0, transform.forward.z);
-            rb.AddForce(moveDir * inputDir.magnitude * moveSpeed * Time.fixedDeltaTime, ForceMode.VelocityChange);
 
-            velocity = rb.velocity;
-            velocity.y = 0;
-            if (Vector3.Dot(velocity.normalized, moveDir) > 0.01f)
-            {
-                velocity = moveDir * velocity.magnitude;
-                velocity.y = rb.velocity.y;
-                rb.velocity = velocity;
-            }
-            velocity.y = 0;
-            if (velocity.magnitude > maxVelocity)
-            {
-                velocity = velocity.normalized * maxVelocity;
-                velocity.y = rb.velocity.y;
-                rb.velocity = velocity;
-            }
+        inputDir = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        rb.rotation = Quaternion.Lerp(rb.rotation, Quaternion.LookRotation(inputDir), turnSpeed * Time.fixedDeltaTime);
+
+        velocity = rb.velocity;
+        moveDir.x = transform.forward.x;
+        moveDir.y = 0;
+        moveDir.z = transform.forward.z;
+        velocity = moveDir * inputDir.magnitude * moveSpeed * Time.fixedDeltaTime;
+        rb.velocity = velocity;
+
+        velocity.y = 0;
+        if (velocity.magnitude > maxVelocity)
+        {
+            velocity = velocity.normalized * maxVelocity;
+            velocity.y = rb.velocity.y;
+            rb.velocity = velocity;
         }
 
     }
