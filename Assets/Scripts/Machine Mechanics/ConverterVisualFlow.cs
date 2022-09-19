bf26f71775957c0c6710a85ec06761ae52a37e67
olyislam <32776200+olyislam@users.noreply.github.com>
2022-09-19 15:35:53 +0600

    Fixed converter flow

--- Assets/Scripts/Machine Mechanics/ConverterVisualFlow.cs
+++ Assets/Scripts/Machine Mechanics/ConverterVisualFlow.cs
@@ -5,65 +5,80 @@ using IdleArcade.Core;
 public class ConverterVisualFlow : MonoBehaviour, TransactionConverter.IConverter
 {
     [SerializeField, Range(0f, 1f)] private float processingDelay = 0.5f;
+    [Header("From Setup")]
     [SerializeField] private Entity fromPrefab;
+    [SerializeField] private Transform fromA;
+    [SerializeField] private Transform fromB;
+    [Header("To Setup")]
     [SerializeField] private Entity convertedPrefab;
-    [SerializeField] private Transform A;
-    [SerializeField] private Transform B;
-    [SerializeField] private Transform C;
+    [SerializeField] private Transform toA;
+    [SerializeField] private Transform toB;
     private GameObject fromVisualElement;
     private GameObject toVisualElement;
 
     private void Awake()
     {
-        fromVisualElement = Instantiate(fromPrefab.gameObject, A.position, A.rotation, transform);
-        fromVisualElement.SetActive(false);
-        toVisualElement = Instantiate(convertedPrefab.gameObject, B.position, B.rotation, transform);
-        toVisualElement.SetActive(false);
+        if (fromPrefab)
+        {
+            fromVisualElement = Instantiate(fromPrefab.gameObject, fromA.position, fromA.rotation, transform);
+            fromVisualElement.SetActive(false);
+        }
+        if (convertedPrefab)
+        { 
+            toVisualElement = Instantiate(convertedPrefab.gameObject, toA.position, toA.rotation, transform);
+            toVisualElement.SetActive(false);
+        }
     }
     public void OnConvertBegin(float delay)
     {
-        StartCoroutine(MoveTo(A.position, B.position, C.position));
+        StartCoroutine(MoveTo(fromA.position, fromB.position, toA.position,toB.position));
 
-        IEnumerator MoveTo(Vector3 A,Vector3 B, Vector3 C)
+        IEnumerator MoveTo(Vector3 fromA,Vector3 fromB, Vector3 toA,Vector3 toB)
         {
             var processingTime = delay * processingDelay;
 
             var swipeDuration = delay - processingTime;
-            var totalPathLength = (C - A).magnitude;
+            var fromLength = Vector3.Distance(fromA, fromB);
+            var toLength = Vector3.Distance(toA, toB);
+            var totalPathLength = fromLength + toLength;
 
-            var pathLength = (A - B).magnitude;
-            var dt = Mathf.InverseLerp(0, totalPathLength, pathLength);
+            var dt = Mathf.InverseLerp(0, totalPathLength, fromLength);
             var startTime = Time.time;
             var endTime = startTime + dt * swipeDuration;
 
             float t;
-            fromVisualElement.SetActive(true);
+            if(fromVisualElement)
+                fromVisualElement.SetActive(true);
             while (Time.time < endTime)
             {
                 t = Mathf.InverseLerp(startTime, endTime, Time.time);
-                fromVisualElement.transform.position = Vector3.Lerp(A, B, t);
+                if(fromVisualElement)
+                    fromVisualElement.transform.position = Vector3.Lerp(fromA, fromB, t);
                 yield return null;
             }
-            fromVisualElement.SetActive(false);
+            if(fromVisualElement)
+                fromVisualElement.SetActive(false);
 
             if (processingTime > 0)
                 yield return new WaitForSeconds(processingTime);
             else
                 yield return null;
 
-            pathLength = (B - C).magnitude;
-            dt = Mathf.InverseLerp(0, totalPathLength, pathLength);
+            dt = Mathf.InverseLerp(fromLength, totalPathLength, toLength);
             startTime = Time.time;
             endTime = startTime + dt * swipeDuration;
 
-            toVisualElement.SetActive(true);
+            if(toVisualElement)
+                toVisualElement.SetActive(true);
             while (Time.time < endTime)
             {
                 t = Mathf.InverseLerp(startTime, endTime, Time.time);
-                toVisualElement.transform.position = Vector3.Lerp(B, C, t);
+                if(toVisualElement)
+                    toVisualElement.transform.position = Vector3.Lerp(toA, toB, t);
                 yield return null;
             }
-            toVisualElement.SetActive(false);
+            if(toVisualElement)
+                toVisualElement.SetActive(false);
         }
     }
 
