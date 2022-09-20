21281ad75cfc545c25c90c6842a820b56a956198
olyislam <32776200+olyislam@users.noreply.github.com>
2022-09-20 13:21:34 +0600

    Delete Extra scripts

--- Assets/Scripts/Machine Mechanics/ElementBuyPoint.cs
+++ Assets/Scripts/Machine Mechanics/ElementBuyPoint.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 using IdleArcade.Core;
 using System.Collections;
+using General.Library;
 
 [RequireComponent(typeof(TriggerDetector))]
-public class MainSource : MonoBehaviour, TriggerDetector.ITriggerable
+public class ElementBuyPoint : MonoBehaviour, TriggerDetector.ITriggerable
 {
- 
-    [SerializeField] private int deltaAmount = 20;
+    [SerializeField] private int buyAmount = 20;
+    [SerializeField] private int unitPrice = 5;
 
+    public int orderAmount = 0;
     private IEnumerator BuyRoutine()
     {
         while (Input.GetMouseButton(0))
             yield return null;
 
-  //      importer.SetOrder(deltaAmount);
+        if (ScoreManager.instance.AddScore(-Mathf.Abs(buyAmount * unitPrice)))
+            orderAmount += buyAmount;
     }
 
     public void OnEnter(Collider other)
