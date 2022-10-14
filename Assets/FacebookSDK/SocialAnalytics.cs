86b2ec06c95d718a35fb9569884473855bc550b8
olyislam <32776200+olyislam@users.noreply.github.com>
2022-10-14 19:06:28 +0600

    FB SDK Implemented

---
+++ Assets/FacebookSDK/SocialAnalytics.cs
@@ -0,0 +1,54 @@
+﻿using Facebook.Unity;
+using UnityEngine;
+
+public class SocialAnalytics : MonoBehaviour
+{
+    private void Awake()
+    {
+        InitilizeFaceook();
+    }
+    
+
+    private void InitilizeFaceook()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                {
+                    FB.ActivateApp();
+#if UNITY_EDITOR
+                    Debug.Log("IsInitialized");
+#endif
+                }
+                else
+                {
+#if UNITY_EDITOR
+                    Debug.Log("Couldn't initialize");
+#endif
+                }
+            },
+            isGameShown =>
+            {
+                if (!isGameShown)
+                {
+#if UNITY_EDITOR
+                    Debug.Log("IS not Game Shown");
+#endif
+                }
+                else
+                {
+#if UNITY_EDITOR
+                    Debug.Log("IS Game Shown");
+#endif
+                }
+            });
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+
+    }
+}
